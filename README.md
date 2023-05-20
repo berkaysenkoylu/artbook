7a67d658074fe2a0812c36be417ec8b79f44748e
berkay <senkoylu_berkay@yahoo.com>
2023-05-20 23:09:13 +0300

    Changed image picker logic and added permission to access user's gallery

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+Android development learning journey #2
+
 A small android app where users can create an art book, where they can add, edit and remove art pieces.
--- app/src/main/java/com/berkaysenkoylu/artbook/AddEditArtActivity.java
+++ app/src/main/java/com/berkaysenkoylu/artbook/AddEditArtActivity.java
@@ -1,27 +1,39 @@
 package com.berkaysenkoylu.artbook;
 
-import androidx.annotation.Nullable;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.berkaysenkoylu.artbook.databinding.ActivityAddEditArtBinding;
-import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.material.snackbar.Snackbar;
 
-import android.app.Activity;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.text.Editable;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.security.Permission;
 import java.sql.SQLOutput;
 
 public class AddEditArtActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> activityResultLauncher;
+    private ActivityResultLauncher<String> permissionLauncher;
 
-    private static final int GALLERY_IMAGE_REQ_CODE = 102;
-    private ImageView imgGallery;
+    private ImageView imgShowcase;
     private String imgUri = "";
+    private Bitmap imgBitmap;
     private int selectedArtId;
     private boolean isEditMode = false;
 
@@ -32,7 +44,9 @@ public class AddEditArtActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityAddEditArtBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        imgGallery = binding.selectedImageView;
+        imgShowcase = binding.selectedImageView;
+
+        registerLaunchers();
 
         Intent intent = getIntent();
         isEditMode = intent.getBooleanExtra("Edit Mode", false);
@@ -41,7 +55,7 @@ public class AddEditArtActivity extends AppCompatActivity {
             selectedArtId = artToEdit.id;
             imgUri = artToEdit.imageUri;
 
-            imgGallery.setImageURI(Uri.parse(imgUri));
+            imgShowcase.setImageURI(Uri.parse(imgUri));
             binding.artNameText.setText(artToEdit.artname);
             binding.painterNameText.setText(artToEdit.painterName);
             binding.dateText.setText(artToEdit.year);
@@ -49,41 +63,84 @@ public class AddEditArtActivity extends AppCompatActivity {
     }
 
     public void onImagePickerPressed(View view) {
-        ImagePicker.with(this)
-                // Crop Image(User can choose Aspect Ratio)
-                .crop()
-                // User can only select image from Gallery
-                .galleryOnly()
+        String permissionStr = "";
+        if (Build.VERSION.SDK_INT >= 33) {
+            permissionStr = Manifest.permission.READ_MEDIA_IMAGES;
+        } else {
+            permissionStr = Manifest.permission.READ_EXTERNAL_STORAGE;
+        }
 
-                .galleryMimeTypes(new String[]{"image/png",
-                        "image/jpg",
-                        "image/jpeg"
-                })
-                // Image resolution will be less than 1080 x 1920
-                .maxResultSize(1080, 1920)
-                // .saveDir(getExternalFilesDir(null))
-                .start(GALLERY_IMAGE_REQ_CODE);
+        if (ContextCompat.checkSelfPermission(this, permissionStr) != PackageManager.PERMISSION_GRANTED) {
+            // Permission has not been granted by the user yet.
+            // Android can decide whether to give the user more info regarding why we need this permission
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissionStr)) {
+                // Show the user the info in a snack bar
+                Snackbar.make(view, "Permission is required to access gallery!", Snackbar.LENGTH_INDEFINITE).setAction("Grant", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Request the permission
+                        String permissionStr = "";
+                        if (Build.VERSION.SDK_INT >= 33) {
+                            permissionStr = Manifest.permission.READ_MEDIA_IMAGES;
+                        } else {
+                            permissionStr = Manifest.permission.READ_EXTERNAL_STORAGE;
+                        }
+                        permissionLauncher.launch(permissionStr);
+                    }
+                }).show();
+            } else {
+                // Request the permission
+                permissionLauncher.launch(permissionStr);
+            }
+
+        } else {
+            // Permission is granted. We can access the gallery now.
+            goToGallery();
+        }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK) {
-            // Uri object will not be null for RESULT_OK
-            Uri uri = data.getData();
-            System.out.println(uri.toString());
+    private void goToGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        activityResultLauncher.launch(galleryIntent);
+    }
 
-            switch (requestCode) {
-                case GALLERY_IMAGE_REQ_CODE:
-                    imgGallery.setImageURI(uri);
-                    imgUri = uri.toString();
-                    break;
+    private void registerLaunchers() {
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK) {
+                    Intent intentFromResult = result.getData();
+                    if (intentFromResult != null) {
+                        Uri uri = intentFromResult.getData();
+
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), uri);
+                                imgBitmap = ImageDecoder.decodeBitmap(source);
+                            } else {
+                                imgBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                            }
+                            binding.selectedImageView.setImageBitmap(imgBitmap);
+                        } catch(Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
             }
-        } else if (resultCode == ImagePicker.RESULT_ERROR) {
-            Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show();
-        }
+        });
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if (result) {
+                    // Permission granted
+                    goToGallery();
+                } else {
+                    // Permission denied
+                    Toast.makeText(AddEditArtActivity.this, "Permission is not granted!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public void onSaveButtonPressed(View view) {
